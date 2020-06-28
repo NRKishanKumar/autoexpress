45d8096b4d0663d5b9141e5f0bc4f9fe985b846c
Kumar <kishan.93k@gmail.com>
2020-06-28 21:39:27 +0530

    Updates

---
+++ precache-manifest.bfd0da524fd10538862ea444776c7758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0344aa02fe735302a74af6ed8546e517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25e2ede0dc878cf73cff",
+    "url": "/static/css/main.f45226ac.chunk.css"
+  },
+  {
+    "revision": "33913abd340ca2883885",
+    "url": "/static/js/2.bc9e813f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc9e813f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e2ede0dc878cf73cff",
+    "url": "/static/js/main.4d9cb3d1.chunk.js"
+  },
+  {
+    "revision": "f84ab788492b1beb3f17",
+    "url": "/static/js/runtime-main.39a9da1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
